852b72ee4dce47c2d200f77ca32d5140a01d5cae
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-09-24 19:39:01 +0200

    Add better_seeds_query and better_tracks_query

---
+++ SQL/n+1 buster/app/models/artist.rb
@@ -0,0 +1,31 @@
+class Artist < ApplicationRecord
+  has_many :albums,
+    class_name: 'Album',
+    foreign_key: :artist_id,
+    primary_key: :id
+
+  def n_plus_one_tracks
+    albums = self.albums
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.title] = album.tracks.length
+    end
+
+    tracks_count
+  end
+
+  def better_tracks_query
+    albums = self.albums
+              .select('albums.title, COUNT(*) AS num_of_tracks')
+              .joins(:tracks)
+              .group('album_id')
+              
+
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.title] = album.num_of_tracks
+    end
+
+    tracks_count
+  end
+end
